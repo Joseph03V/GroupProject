f08b6d748ed9108651886a74073c14b37cb905d7
Nicholas Hudson <nick.hudson512@gmail.com>
2023-10-30 21:47:05 -0500

    localStorage function for myPawfile

--- script.js
+++ script.js
@@ -4,19 +4,36 @@ goldie.style.display = 'none'
 
 function getApi() {
     let dogApi = 'https://www.mapquestapi.com/geocoding/v1/address?key=baOsBcecBVf88PhrMAJAWa7jQppd2hIV&location=Washington,DC'
-     
-
+    
+    
     fetch(dogApi)
     .then(function (response){
         return response.json()
     })
-   .then(function(data) {
-    console.log(data)
-
-
-})
+    .then(function(data) {
+        console.log(data)
+        
+        
+    })
 }
 function showPic() {
     goldie.style.display = 'block'
 }
 $('goldie').toggleClass('click', showPic)
+
+document.getElementById("profileForm").addEventListener("submit", function(event){
+event.preventDefault()
+    var name = document.getElementById("name").value
+    var age = document.getElementById("age").value
+    var gender= document.getElementById("gender").value
+    var interests = document.getElementById("interests").value
+
+    var formData = {
+        name: name,
+        age: age,
+        gender: gender,
+        interests: interests
+    }
+
+    localStorage.setItem("profileData", JSON.stringify(formData))
+})
