4b87a3e0de884e8163769e9c300ff5cebcd21d8b
Martin Renteria <mrrenteria0159@gmail.com>
2023-10-30 22:32:59 -0600

    moved js code to file for pawfile

--- script.js
+++ script.js
@@ -45,19 +45,3 @@ $('golden').toggleClass('click', showPic)
     
  
 
-document.getElementById("profileForm").addEventListener("submit", function(event){
-event.preventDefault()
-    var name = document.getElementById("name").value
-    var age = document.getElementById("age").value
-    var gender= document.getElementById("gender").value
-    var interests = document.getElementById("interests").value
-
-    var formData = {
-        name: name,
-        age: age,
-        gender: gender,
-        interests: interests
-    }
-
-    localStorage.setItem("profileData", JSON.stringify(formData))
-})
