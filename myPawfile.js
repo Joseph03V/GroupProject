c8fa351cf1e05f5e63cacf078589de9a99c3173f
Nicholas Hudson <nick.hudson512@gmail.com>
2023-11-02 18:05:55 -0500

    use current location function

--- myPawfile.js
+++ myPawfile.js
@@ -2,6 +2,7 @@ let create = document.getElementById('create')
 let profilePic = document.getElementById('profile-pic')
 let inputFile = document.getElementById('input-file')
 profileInfo = document.getElementById("profileForm")
+
 profileInfo.addEventListener("submit", function(event){
     event.preventDefault()
     var name = document.getElementById("name").value
@@ -21,24 +22,43 @@ profileInfo.addEventListener("submit", function(event){
 function createPro() {
     create.style.display = 'none'
 }
-create.addEventListener('click', createPro)
+create.addEventListener("click", createPro)
 inputFile.onchange = function() {
     profilePic.src = URL.createObjectURL(inputFile.files[0])
 }
 
+function initMap(latitude, longitude) {
+    L.mapquest.key = 'wDqE9BnvH5OgxagEI2OQqf5lI1s4yZ12';
+    var map = L.mapquest.map('map', {
+        center: [latitude, longitude],
+        layers: L.mapquest.tileLayer('map'),
+        zoom: 12
+    });
 
-    // L.marker([50.505, 30.57], {icon: myIcon}).addTo(map);
-    
-    //   function icon(){
-        //     let link = 'https://assets.mapquestapi.com/icon/v2/marker@2x.png'
-        //     L.mapquest.key = 'wDqE9BnvH5OgxagEI2OQqf5lI1s4yZ12';
-        
-//     fetch(link)
-//     .then(function(response){
-    //         return response.json()
-    //     })
-    //   }
-    
-    //   icon();
-    
-    
+    var myIcon = L.icon({
+        iconUrl: 'https://assets.mapquestapi.com/icon/v2/marker@2x.png',
+        iconSize: [38, 48],
+        iconAnchor: [22, 94],
+        popupAnchor: [-3, -76]
+    });
+
+    L.marker([latitude, longitude], { icon: myIcon }).addTo(map);
+    map.addControl(L.mapquest.control());
+}
+
+function getCoordinates() {
+    navigator.geolocation.getCurrentPosition(
+        function(position) {
+            var latitude = position.coords.latitude;
+            var longitude = position.coords.longitude;
+            initMap(latitude, longitude);
+        },
+        function(error) {
+            console.log(error);
+            alert("Failed to retrieve your location!");
+        }
+    );
+}
+
+var locationButton = document.getElementById("location");
+locationButton.addEventListener("click", getCoordinates);
